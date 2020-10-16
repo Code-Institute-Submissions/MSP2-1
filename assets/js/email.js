5d9280dc8ed5e7ef8bb2152b888bdd370163ed5a
matildamuyingo <matilda.muyingo@hotmail.com>
2020-10-16 10:26:21 +0000

    Added the email function, the maps showing and put the maps function in the Card results file. The maps information is updated to be conditional for the options picked

---
+++ assets/js/email.js
@@ -0,0 +1,19 @@
+
+function sendMail(contactForm) {
+    emailjs.send("gmail", "elitetravel", {
+        "from_name": contactForm.firstname.value+" "+ contactForm.lastname.value,
+        "from_email": contactForm.emailadress.value,
+        "phone_number": contactForm.phonenumber.value,
+        "destination": contactForm.destination.value,
+        "dates": contactForm.bookingdates.value,
+        "message": contactForm.bookingrequest.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        });
+    return false;
+}
--- assets/js/resultCards.js
+++ assets/js/resultCards.js
@@ -1,8 +1,52 @@
-function checkValue() {
 
-       
-        var country = ["USA","Thailand","Morocco","Austrailia","Bahamas","Spain","France","Spain","Brazil","Sweden","Switzerland","Ecuador","Canada","Norway","Czech Republic","Germany","UAE","Japan","Austria","Italy","Iceland","Peru","Zimbabwe"];
-        var city = [["Florida","Bankok","Marakech","Port Douglas","Nassau","Tenerife","Nice","Barcelona","Rio de Janeiro"],["Åre","St. Moritz","Duluth - Minnesota","Mt. Cayambe","Banff","Kiruna","Bergen","Quebec City","Prague"],["Paris","Munich","Dubai","Tokyo","Vienna","Venice","Reykjavik","Cuzco","Hwange National Park"]];
+
+var countryInfo = [["USA","Miami", 25.761681, -80.191788],["Thailand", "Bankok", 13.736717, 100.523186],["Morocco", "Marakech", 31.669746, -7.973328],["Austrailia","Port Douglas", -16.4835, 145.4652],["Bahamas","Nassau", 25.059999, -77.345001], ["Spain", "Tenerife", 28.291565, -16.629129], ["France", "Nice", 43.675819, 7.289429], ["Spain", "Barcelona", 41.390205, 2.154007], ["Brazil", "Rio de Janeiro", -22.908333, -43.196388], ["Sweden", "Åre", 63.245400, 13.034320], ["Switzerland", "St. Moritz", 46.4984, 9.8391], ["USA", "Duluth MN", 46.471201, 92.617532], ["Ecuador", "Mt. Cayambe", 0.0290, -77.9860], ["Canada", "Banff", 51.180202, -115.565704], ["Norway", "Bergen",  60.397076, 5.324383], ["Czech Republic", "Prague", 50.073658, 14.418540], ["Germany", "Berlin", 52.520008, 13.404954], ["UAE", "Dubai", 25.276987, 55.296249], ["Japan", "Tokyo", 35.658581, 139.745438], ["Austria", "Vienna", 48.210033, 16.363449], ["Italy", "Venice", 45.444958, 12.328463], ["Iceland", "Reykjavik", 64.128288, 21.827774], ["Peru", "Cuzco", 13.52500, 71.97222], ["Zimbabwe", "Hwange National Park", -18.6247, 27.0193]];        
+
+function initMap(a,b,c,d,e,f) {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: {
+            lat: e,
+            lng: f,
+        }
+    });
+
+    var map2 = new google.maps.Map(document.getElementById("map2"), {
+        zoom: 7,
+        center: {
+            lat: c,
+            lng: d,
+        }
+    });
+
+    var map3 = new google.maps.Map(document.getElementById("map3"), {
+        zoom: 7,
+        center: {
+            lat: a,
+            lng: b,
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+
+    var locations = [
+        { lat: 40.785091, lng: -73.968285},
+        { lat: 41.084045, lng: -73.874245},
+        { lat: 40.754932, lng: -73.984016},
+        ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+            });
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    }
+
+    function checkValue() {
 
         var destSubmit = $("#inline-link-input").val();
 
@@ -22,30 +66,38 @@ function checkValue() {
                     
                     if (data2 == "Shopping") {
                         
-                        $("#card-head-1").html(country[0] +", "+city[0][0]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[1] +", "+city[0][1]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[2] +", "+city[0][2]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[0][1] +", "+countryInfo[0][0]);
+                        
+                        
+                        $("#card-head-2").html(countryInfo[1][1] +", "+countryInfo[1][0]);
+                        
+
+                        $("#card-head-3").html(countryInfo[2][1] +", "+countryInfo[2][0]);
+                        
+
+                        initMap(countryInfo[0][2], countryInfo[0][3],countryInfo[1][2],countryInfo[1][3],countryInfo[2][2],countryInfo[2][3]);
                     
                     } else if (data2 == "Sun-Bathing") {
                         
-                        $("#card-head-1").html(country[3] +", "+city[0][3]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[4] +", "+city[0][4]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[5] +", "+city[0][5]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[3][1] +", "+countryInfo[3][0]);
+                        
+                        $("#card-head-2").html(countryInfo[4][1] +", "+countryInfo[4][0]);
+                        
+                        $("#card-head-3").html(countryInfo[5][1] +", "+countryInfo[5][0]);
+                        
+
+                        initMap(countryInfo[3][2], countryInfo[3][3],countryInfo[4][2],countryInfo[4][3],countryInfo[5][2],countryInfo[5][3]);
                     
                     } else if (data2 == "Culture") {
                         
-                        $("#card-head-1").html(country[6] +", "+city[0][6]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[7] +", "+city[0][7]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[8] +", "+city[0][8]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[6][1] +", "+countryInfo[6][0]);
+                        
+                        $("#card-head-2").html(countryInfo[7][1] +", "+countryInfo[7][0]);
+                        
+                        $("#card-head-3").html(countryInfo[8][1] +", "+countryInfo[8][0]);
+                        
+
+                        initMap(countryInfo[6][2], countryInfo[6][3],countryInfo[7][2],countryInfo[7][3],countryInfo[8][2],countryInfo[8][3]);
                     
                     }
                     
@@ -55,31 +107,37 @@ function checkValue() {
                     console.log(data);
                     if (data2 == "Skiing") {
                         
-                        $("#card-head-1").html(country[9] +", "+city[1][0]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[10] +", "+city[1][1]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[0] +", "+city[1][2]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[9][1] +", "+countryInfo[9][0]);
+                        
+                        $("#card-head-2").html(countryInfo[10][1] +", "+countryInfo[10][0]);
+                        
+                        $("#card-head-3").html(countryInfo[11][1] +", "+countryInfo[11][0]);
+                        
+
+                        initMap(countryInfo[9][2], countryInfo[9][3],countryInfo[10][2],countryInfo[10][3],countryInfo[11][2],countryInfo[11][3]);
                     
                     } else if (data2 == "Hiking") {
                         
-                        $("#card-head-1").html(country[11] +", "+city[1][3]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[12] +", "+city[1][4]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[9] +", "+city[1][5]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[12][1] +", "+countryInfo[12][0]);
+                        
+                        $("#card-head-2").html(countryInfo[13][1] +", "+countryInfo[13][0]);
+                        
+                        $("#card-head-3").html(countryInfo[14][1] +", "+countryInfo[14][0]);
+                        
+
+                        initMap(countryInfo[12][2], countryInfo[12][3],countryInfo[13][2],countryInfo[13][3],countryInfo[14][2],countryInfo[14][3]);
                     
                     } else if (data2 == "Sleading") {
                         
-                        $("#card-head-1").html(country[13] +", "+city[1][6]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[12] +", "+city[1][7]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[14] +", "+city[1][8]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[15][1] +", "+countryInfo[15][0]);
+                        
+                        $("#card-head-2").html(countryInfo[16][1] +", "+countryInfo[16][0]);
+                        
+                        $("#card-head-3").html(countryInfo[17][1] +", "+countryInfo[17][0]);
+                        
                     
+                        initMap(countryInfo[15][2], countryInfo[15][3],countryInfo[16][2],countryInfo[16][3],countryInfo[17][2],countryInfo[17][3]);
+
                     }
                     
                 }
@@ -88,30 +146,36 @@ function checkValue() {
                     console.log(data);
                     if (data2 == "Hiking") {
                         
-                        $("#card-head-1").html(country[6] +", "+city[2][0]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[15] +", "+city[2][1]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[16] +", "+city[2][2]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[18][1] +", "+countryInfo[18][0]);
+                        
+                        $("#card-head-2").html(countryInfo[19][1] +", "+countryInfo[19][0]);
+                        
+                        $("#card-head-3").html(countryInfo[20][1] +", "+countryInfo[20][0]);
+                        
+
+                        initMap(countryInfo[18][2], countryInfo[18][3],countryInfo[19][2],countryInfo[19][3],countryInfo[20][2],countryInfo[20][3]);
                     
                     } else if (data2 == "Culture") {
                         
-                        $("#card-head-1").html(country[17] +", "+city[2][3]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[18] +", "+city[2][4]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[19] +", "+city[2][5]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[21][1] +", "+countryInfo[21][0]);
+                        
+                        $("#card-head-2").html(countryInfo[22][1] +", "+countryInfo[22][0]);
+                        
+                        $("#card-head-3").html(countryInfo[23][1] +", "+countryInfo[23][0]);
+                        
+
+                        initMap(countryInfo[21][2], countryInfo[21][3],countryInfo[22][2],countryInfo[22][3],countryInfo[23][2],countryInfo[23][3]);
                     
                     } else if (data2 == "Adventure") {
                         
-                        $("#card-head-1").html(country[20] +", "+city[2][6]);
-                        console.log(data2);
-                        $("#card-head-2").html(country[21] +", "+city[2][7]);
-                        console.log(data2);
-                        $("#card-head-3").html(country[22] +", "+city[2][8]);
-                        console.log(data2);
+                        $("#card-head-1").html(countryInfo[24][1] +", "+countryInfo[24][0]);
+                        
+                        $("#card-head-2").html(countryInfo[25][1] +", "+countryInfo[25][0]);
+                        
+                        $("#card-head-3").html(countryInfo[26][1] +", "+countryInfo[26][0]);
+                        
+
+                        initMap(countryInfo[24][2], countryInfo[24][3],countryInfo[25][2],countryInfo[25][3],countryInfo[26][2],countryInfo[26][3]);
                     
                     }
                     
@@ -124,4 +188,5 @@ function checkValue() {
         
     
     }
+
 
